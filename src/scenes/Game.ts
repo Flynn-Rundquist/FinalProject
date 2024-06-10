9581ff278736162a393d6067a32aa04caf1d8da4
Flynn Rundquist <flynn.rundquist@mths.ca>
2024-06-10 10:02:30 -0400

    sprites move!!

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -2,59 +2,62 @@ import Phaser from 'phaser';
 import Player from '../sprites/Player';
 import Enemy from '../sprites/Enemy';
 
-export class Game extends Phaser.Scene
-{
-    camera: Phaser.Cameras.Scene2D.Camera;
-    background: Phaser.GameObjects.Image;
-    msg_text : Phaser.GameObjects.Text;
+export class Game extends Phaser.Scene {
+    private camera: Phaser.Cameras.Scene2D.Camera;
+    private background: Phaser.GameObjects.Image;
+    private msg_text: Phaser.GameObjects.Text;
 
-    player: Phaser.GameObjects.Sprite | null
-    enemy: Phaser.GameObjects.Sprite | null
-    platforms: Phaser.Physics.Arcade.StaticGroup | null
-    coin: Phaser.Physics.Arcade.StaticGroup | null
-    score: number
+    private player: Player | null = null;
+    private platforms: Phaser.Physics.Arcade.StaticGroup | null = null;
+    private coin: Phaser.Physics.Arcade.StaticGroup | null = null;
+    private score: number = 0;
 
-    constructor()
-    {
+    constructor() {
         super('Game');
-
-        this.player = null;
-        this.enemy = null;
-        this.platforms = null;
-        this.coin = null;
-        this.score = 0;
-
     }
 
-    config: Phaser.Types.Scenes.SettingsConfig;
-
-    create()
-    {
+    create() {
         this.camera = this.cameras.main;
         this.camera.setBounds(0, 0, 1024, 576);
-        
-        // make player 
-        this.player = this.add.sprite(50, 100, 'playerSprite');
-        let player = new Player({ scene: this, x: 100, y: 435, key: 'playerSprite' }, 100, 0);
 
-        // make enemy (random x, 450 y)
-        this.enemy = this.add.sprite(10, 100, 'enemySprite');
-        let enemy = new Enemy({ scene: this, x: Enemy.randomX(), y: 435, key: 'enemySprite' }, 10);
-
+        // Add background
         this.background = this.add.image(0, 0, 'gameBG').setOrigin(0, 0);
         this.background.setAlpha(1);
 
-        // add player sprite
-        Player.addPlayer(this, player);
+        // set the world bounds so the player can't go below y = 435
+        this.physics.world.setBounds(0, 0, 1024, 440);
 
-        // every 10 seconds, add an enemy to the scene
+        // Create player and add to scene
+        this.player = new Player({
+            scene: this,
+            x: 100,
+            y: 450,
+            texture: 'playerSprite'
+        }, 100, 0);
+
+        // Add player to the scene
+        this.add.existing(this.player);
+
+        // Create enemy and add to scene periodically
         this.time.addEvent({
             delay: 10000,
             callback: () => {
-                enemy.addEnemy(this, enemy);
+                const enemyX = Enemy.randomX(); // Get random x position for enemy
+                const enemyY = 435; // Set y position of the enemy
+                const enemy = Enemy.addEnemy(this, enemyX, enemyY, 'enemySprite', 10); // Create and add enemy
+                
+                // get enemy to shoe up on scene
+                this.add.existing(enemy);
             },
             loop: true
         });
+    }
 
+    update() {
+        if (this.player) {
+            this.player.update();
+        }
     }
 }
+
+export default Game;
--- src/sprites/Enemy.ts
+++ src/sprites/Enemy.ts
@@ -1,75 +1,60 @@
-/**
- * Enemy class
- *
- * By: Flynn Rundquist
- * Version: 1.0
- * Since: 2024-06-06
- */
-
-import { GameObjects } from 'phaser';
+import { Physics } from 'phaser';
 import Player from './Player';
 
-class Enemy extends GameObjects.Sprite
-{
-    protected health: number = 10;
+class Enemy extends Physics.Arcade.Sprite {
+    health: number = 10;
 
-    // The constructor initializes the enemy sprite.
-    constructor (config: { scene: Phaser.Scene, x: number, y: number, key: string }, health: number)
-    {
+    constructor(config: { scene: Phaser.Scene; x: number; y: number; key: string }, health: number) {
         super(config.scene, config.x, config.y, config.key);
         this.health = health;
+
+        // Enable physics
+        config.scene.physics.world.enable(this);
+
+        // Set collide world bounds
+        this.setCollideWorldBounds(true);
     }
 
-    // add enemy to the scene
-    public addEnemy(scene: Phaser.Scene, enemy: Enemy)
-    {
+    // Add enemy to the scene
+    static addEnemy(scene: Phaser.Scene, x: number, y: number, key: string, health: number) {
+        const enemy = new Enemy({ scene, x, y, key }, health);
         scene.add.existing(enemy);
-        scene.physics.add.existing(enemy);
+        return enemy;
     }
 
-    // generate random x value for enemy (between 500 and 924)
-    static randomX()
-    {
+    // Generate random x value for enemy (between 500 and 924)
+    static randomX() {
         return Math.floor(Math.random() * (924 - 500) + 500);
     }
 
-    // Makes the enemy sprite move towards player if they're within 100 pixels 
-    public wakeUp (player: Player)
-    {
-        if (this.x - player.x < 100)
-        {
+    // Makes the enemy sprite move towards player if they're within 100 pixels
+    wakeUp(player: Player) {
+        if (this.x - player.x < 100) {
             this.x -= 5;
         }
     }
 
-    // if enemey hits player, player loses health
-    public hitPlayer (player: Player)
-    {
-        if (this.x - player.x < 10)
-        {
+    // If enemy hits player, player loses health
+    hitPlayer(player: Player) {
+        if (this.x - player.x < 10) {
             player.health -= 10;
         }
     }
 
-
-    // if player hits enemy, enemy loses health
-    public hitEnemy(player: Player, cursors: Phaser.Types.Input.Keyboard.CursorKeys) {
-        if (player.x - this.x < 10 && cursors.space.isDown)
-        {
+    // If player hits enemy, enemy loses health
+    hitEnemy(player: Player, cursors: Phaser.Types.Input.Keyboard.CursorKeys) {
+        if (player.x - this.x < 10 && cursors.space.isDown) {
             this.health -= 10;
             player.score += 10;
         }
     }
 
-
-    // if the enemies health goes to 0, destroy the sprite
-    public dead ()
-    {
-        if (this.health == 0)
-        {
+    // If the enemy's health goes to 0, destroy the sprite
+    dead() {
+        if (this.health === 0) {
             this.destroy();
         }
     }
-
 }
+
 export default Enemy;
